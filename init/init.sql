46ea41eaedf9e8c7fcf5adcd872305d518ce6d37
kristian <gardosik.k@gmail.com>
2025-08-06 10:24:23 +0200

    Vymazanie postgres containera. Úprava tabuliek pre Use Case.

--- init/init.sql
+++ init/init.sql
@@ -1,33 +1,69 @@
-CREATE TABLE owners (
-  owner_id SERIAL PRIMARY KEY,
-  owner VARCHAR(50),
-  house_number NOT NULL,
-  city VARCHAR(50),
-  number_of_inhabitants INT NOT NULL 
+CREATE TABLE osoba (
+  id SERIAL PRIMARY KEY,
+  meno VARCHAR(50),
+  priezvisko VARCHAR(100),
+  datum narodenia DATE,
+  majitel BOOLEAN,
+  id_dom INT,
+  FOREIGN KEY (id_dom) REFERENCES dom(id) 
 );
 
-INSERT INTO users (owner, house_number, city,number_of_inhabitants)
-VALUES 
-('Kristián Gardošík',45,'Slatvina',6),
-('Zuzana Sedláková',98,'Olcnava',4),
-('Janka Pavlíková',14,'Olcnava',4),
-('Anna Nehilová',87,'Slatvina',4),
-('Gregor Sedlák',62,'Krompachy',0);
 
-CREATE TABLE houses  (
-  id_house SERIAL PRIMARY KEY,
-  owner VARCHAR(50) REFERENCES owners(owner),
-  number_of_rooms INT NOT NULL,
-  color VARCHAR(20),
-  garden BOOLEAN;
-)
+CREATE TABLE dom (
+  id SERIAL PRIMARY KEY,
+  ulica VARCHAR (100),
+  číslo_domu SMALLINT,
+  mesto VARCHAR (100),
+  farba VARCHAR(20),
+  zahrada BOOLEAN
+);
 
-INSERT INTO houses (owner. number_of_rooms, color, garden)
-VALUES
 
-('Kristián Gardošík',3,'red',TRUE),
-('Zuzana Sedláková',5,'silver',TRUE),
-('Janka Pavlíková',4,'white',TRUE),
-('Anna Nehilová',5,'white',TRUE),
-('Gregor Sedlák',3,'orange',FALSE);
+CREATE TABLE izba (
+  id SERIAL PRIMARY KEY,
+  typ VARCHAR(30),
+  pocet_okien SMALLINT,
+  vykurovanie VARCHAR(30)
+  id_dom INT,
+  id_osoba INT,
+  FOREIGN KEY (id_dom) REFERENCES dom(id),
+  FOREIGN KEY (id_osoba) REFERENCES osoba(id)
 
+);
+
+CREATE TABLE zahrada (
+  Výmer_v_m2 INT
+  druh VARCHAR(50),
+  id_dom INT,
+  FOREIGN KEY (id_dom) REFERENCES dom(id)
+);
+
+
+/*
+zoznam moznych zariadenie
+1. skirna
+2. postel
+3. stol
+*/
+
+CREATE TABLE zariadenie (
+  id SERIAL PRIMARY KEY,
+  typ VARCHAR(50),
+  cena DECIMAL(8,2)
+);
+
+
+
+
+CREATE TABLE zariadenie_izby (
+id INT,
+id_izba INT,
+id_zariadenie INT,
+pocet INT,
+FOREIGN KEY (id_izba) REFERENCES izba (id),
+FOREIGN KEY (id_zariadenia) REFERENCES zariadenie(id)
+);
+
+
+
+/*Ako sa definuje foreign key a primary key*/
