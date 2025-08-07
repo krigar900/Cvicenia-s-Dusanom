5d0834aa1526ad4e966f6982980010d521c027d1
kristian <gardosik.k@gmail.com>
2025-08-07 11:04:25 +0200

    Oprava DDL prikazov a pridanie dát do databázy

--- init/rodinne_domy_ddl.sql
+++ init/rodinne_domy_ddl.sql
@@ -1,49 +1,3 @@
-CREATE TABLE osoba (
-  id SERIAL PRIMARY KEY,
-  meno VARCHAR(50),
-  priezvisko VARCHAR(100),
-  datum narodenia DATE,
-  majitel BOOLEAN,
-  id_dom INT,
-  id_rodina INT,
-  FOREIGN KEY (id_dom) REFERENCES dom(id),
-  FOREIGN KEY (id_rodina) REFERENCES rodina(id) 
-);
-
-
-CREATE TABLE dom (
-  id SERIAL PRIMARY KEY,
-  ulica VARCHAR (100),
-  číslo_domu SMALLINT,
-  mesto VARCHAR (100),
-  farba VARCHAR(20),
-  zahrada BOOLEAN
-  id_rodina INT,
-  FOREIGN KEY (id_rodina) REFERENCES rodina(id)
-);
-
-
-CREATE TABLE izba (
-  id SERIAL PRIMARY KEY,
-  typ VARCHAR(30),
-  pocet_okien SMALLINT,
-  vykurovanie VARCHAR(30)
-  id_dom INT,
-  id_osoba INT,
-  FOREIGN KEY (id_dom) REFERENCES dom(id),
-  FOREIGN KEY (id_osoba) REFERENCES osoba(id)
-);
-
-
-CREATE TABLE zahrada (
-  Výmer_v_m2 INT
-  druh VARCHAR(50),
-  id_dom INT,
-  FOREIGN KEY (id_dom) REFERENCES dom(id)
-);
-
-
-
 --zoznam moznych zariadenie--
 --1. skirna--
 --2. postel--
@@ -57,6 +11,54 @@ CREATE TABLE zariadenie (
 );
 
 
+CREATE TABLE dom (
+  id SERIAL PRIMARY KEY,
+  ulica VARCHAR (100),
+  cislo_domu SMALLINT,
+  mesto VARCHAR (100),
+  farba VARCHAR(20),
+  zahrada BOOLEAN,
+  id_rodina INT,
+  );
+
+CREATE TABLE osoba (
+  id SERIAL PRIMARY KEY,
+  meno VARCHAR(50),
+  priezvisko VARCHAR(100),
+  datum_narodenia DATE,
+  majitel BOOLEAN,
+  id_dom INT,
+  id_rodina INT,
+  FOREIGN KEY (id_dom) REFERENCES dom(id),
+  FOREIGN KEY (id_rodina) REFERENCES rodina(id) 
+);
+
+CREATE TABLE rodina (
+  id SERIAL PRIMARY KEY,
+  meno VARCHAR(30),
+  pocet clenov INT,
+  id_dom INT,
+  FOREIGN KEY (id_dom) REFERENCES dom(id)
+);
+
+CREATE TABLE izba (
+  id SERIAL PRIMARY KEY,
+  typ VARCHAR(30),
+  pocet_okien SMALLINT,
+  vykurovanie VARCHAR(30),
+  id_dom INT,
+  id_osoba INT,
+  FOREIGN KEY (id_dom) REFERENCES dom(id),
+  FOREIGN KEY (id_osoba) REFERENCES osoba(id)
+);
+
+CREATE TABLE zahrada (
+  id SERIAL PRIMARY KEY
+  Výmer_v_m2 INT,
+  druh VARCHAR(50),
+  id_dom INT,
+  FOREIGN KEY (id_dom) REFERENCES dom(id)
+);
 
 CREATE TABLE zariadenie_izby (
 id SERIAL PRIMARY KEY,
@@ -64,15 +66,9 @@ id_izba INT,
 id_zariadenie INT,
 pocet INT,
 FOREIGN KEY (id_izba) REFERENCES izba (id),
-FOREIGN KEY (id_zariadenia) REFERENCES zariadenie(id)
+FOREIGN KEY (id_zariadenie) REFERENCES zariadenie(id)
 );
 
 
-CREATE TABLE rodina (
-  id SERIAL PRIMARY KEY,
-  meno VARCHAR(30),
-  počet členov INT,
-  id_dom INT,
-  FOREIGN KEY (id_dom) REFERENCES dom(id)
-);
+
 
---
+++ init/rodinne_domy_values.sql
@@ -0,0 +1,145 @@
+
+--Data tabulky zariadenie
+INSERT INTO zariadenie
+(id, typ, cena)
+VALUES
+    (nextval('zariadenie_id_seq'::regclass), 'Stol', 200.50),  
+    (nextval('zariadenie_id_seq'::regclass), 'Stolicka', 50.45),  
+    (nextval('zariadenie_id_seq'::regclass), 'Postel', 400.50),  
+    (nextval('zariadenie_id_seq'::regclass), 'Kreslo', 120.35),  
+    (nextval('zariadenie_id_seq'::regclass), 'Pohovka', 320.85),  
+    (nextval('zariadenie_id_seq'::regclass), 'Skrina', .150.51),  
+    (nextval('zariadenie_id_seq'::regclass), 'Televizia', 450.30),  
+    (nextval('zariadenie_id_seq'::regclass), 'Sporak', 320.50),  
+    (nextval('zariadenie_id_seq'::regclass), 'Polica', 30.45),  
+    (nextval('zariadenie_id_seq'::regclass), 'Komoda', 80.31),  
+    (nextval('zariadenie_id_seq'::regclass), 'Zrkadlo', 90.60);  
+
+--Data tabulky rodina--
+INSERT INTO rodina
+(id, meno, pocet_clenov)
+VALUES
+    (nextval('rodina_id_seq'::regclass), 'Sedlakovci', 5),
+    (nextval('rodina_id_seq'::regclass), 'Nehilovci', 5),
+    (nextval('rodina_id_seq'::regclass), 'Pavlikovci', 4),
+    (nextval('rodina_id_seq'::regclass), 'Gardosijovci', 6),
+
+--Data tabulky dom--
+
+INSERT INTO dom
+(id, ulica, cislo_domu, mesto, farba, zahrada, id_rodina)
+VALUES
+    (nextval('dom_id_seq'::regclass), '-', 45, 'Slatvina', 'bordova', true, 0), --kristian--
+    (nextval('dom_id_seq'::regclass), '-', 45, 'Slatvina', 'bordova', true, 0), --mamka/ocko--
+    (nextval('dom_id_seq'::regclass), '-', 87, 'Slatvina', 'biela', true, 0),   --anicka--
+    (nextval('dom_id_seq'::regclass), 'Osloboditelov', 95, 'Olcnava', 'ruzova', true, 0), --zuzana--
+    (nextval('dom_id_seq'::regclass), 'Hornadska', 5, 'Olcnava', 'siva', true, 0), --janka--
+    (nextval('dom_id_seq'::regclass), 'Maurerova',793, 'Krompachy', 'oranova', true, 0); --gregor--
+
+
+--Data tabulky osoba--
+
+INSERT INTO osoba
+(id, meno, priezvisko, datum_narodenia, majitel, id_dom, id_rodina)
+VALUES
+--Sedlakovci--
+    (nextval('osoba_id_seq'::regclass), 'Zuzana', 'Sedlakova', '1975-05-24', true, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Robert', 'Sedlak', '1973-06-12', true, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Gregor', 'Sedlak', '1999-02-15', false, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Jan', 'Sedlak', '2001-01-06', false, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Matej', 'Sedlak', '2002-08-13', false, 0, 0),
+--Nehilovci--
+    (nextval('osoba_id_seq'::regclass), 'Anna', 'Nehilova', '1976-06-21', true, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Milos', 'Nehila', '1975-07-31', true, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Flavian', 'Nehila', '2001-12-06', false, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Bystrik', 'Nehila', '2012-08-13', false, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Dorotka', 'Nehilova', '2014-03-14', false, 0, 0),
+--Pavlikovci--
+    (nextval('osoba_id_seq'::regclass), 'Jane', 'Pavlikova', '1977-09-12', true, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Marek', 'Pavlik', '1975-10-13', true, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Nina', 'Pavlikova', '2011-4-12', false, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Vratko', 'Pavlik', '2015-5-12', false, 0, 0),
+--Gardosikovci--
+    (nextval('osoba_id_seq'::regclass), 'Anna', 'Gardosikova', '1952-01-06', false, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Jan', 'Gardosikov', '1953-03-24', false, 0, 0),        
+    (nextval('osoba_id_seq'::regclass), 'Michaela', 'Gardosikova', '1992-04-02', false, 0, 0),
+    (nextval('osoba_id_seq'::regclass), 'Kristian', 'Gardosik', '1993-09-09', true, 0, 0),   
+    (nextval('osoba_id_seq'::regclass), 'Damian', 'Gardosik', '2017-12-14', false, 0, 0), 
+    (nextval('osoba_id_seq'::regclass), 'Benjamin', 'Gardosik', '2022-5-19', false, 0, 0);            
+
+
+
+
+--Data tabulky izba--
+
+INSERT INTO izba
+(id, typ, pocet_okien, vykurovanie, id_dom, id_osoba)
+VALUES
+--Sedlakovci--
+    (nextval('izba_id_seq'::regclass), 'kuchyna', 2, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'obyvacka', 3, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'spalna', 1, 'teplovodne', 0, 0), --Zuzana,Robo--
+    (nextval('izba_id_seq'::regclass), 'detska', 1, 'teplovodne', 0, 0), --Gregor--
+    (nextval('izba_id_seq'::regclass), 'pracovna', 1, 'teplovodne', 0, 0),
+--Nehilovci--
+    (nextval('izba_id_seq'::regclass), 'kuchyna', 2, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'obyvacka', 2, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'spalna', 1, 'teplovodne', 0, 0), --Anna, Milos--
+    (nextval('izba_id_seq'::regclass), 'detska', 1, 'teplovodne', 0, 0), --Dorotka--
+    (nextval('izba_id_seq'::regclass), 'detska', 2, 'teplovodne', 0, 0), --Flavian--
+    (nextval('izba_id_seq'::regclass), 'detska', 1, 'teplovodne', 0, 0), --Bystrik--
+--Pavlikovci--
+    (nextval('izba_id_seq'::regclass), 'kuchyna', 3, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'obyvacka', 1, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'spalna', 1, 'teplovodne', 0, 0), --Janka,Marek--
+    (nextval('izba_id_seq'::regclass), 'detska', 3, 'teplovodne', 0, 0), --Vratko--
+    (nextval('izba_id_seq'::regclass), 'detska', 4, 'teplovodne', 0, 0), --Nina--
+--Gardosikovci ml.--
+    (nextval('izba_id_seq'::regclass), 'kuchyna', 1, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'obyvacka',3, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'spalna', 1, 'teplovodne', 0, 0), --Miska,Kristian--
+    (nextval('izba_id_seq'::regclass), 'detska', 2, 'teplovodne', 0, 0), --Fsmisn, Benjamin--
+--Gardosikovci st.--
+    (nextval('izba_id_seq'::regclass), 'kuchyna', 1, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'obyvacka', 0, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'spalna', 2, 'teplovodne', 0, 0), --Jan, Anna--
+--Gregor--
+    (nextval('izba_id_seq'::regclass), 'kuchyna', 1, 'teplovodne', 0, 0),
+    (nextval('izba_id_seq'::regclass), 'obyvacka', 2, 'teplovodne', 0, 0), --Matej--
+    (nextval('izba_id_seq'::regclass), 'spalna', 2, 'teplovodne', 0, 0); --Jan--
+    
+ 
+ --Data tabulky zahrada--
+ INSERT INTO zahrada
+(id, vymer_v_m2, druh, id_dom)
+VALUES
+    (nextval('zahrada_id_seq'::regclass), 350, 'uzitkova', 0),
+    (nextval('zahrada_id_seq'::regclass), 200, 'uzitkova', 0),
+    (nextval('zahrada_id_seq'::regclass), 150, 'okrasna', 0),
+    (nextval('zahrada_id_seq'::regclass), 45, 'okrasna', 0),
+    (nextval('zahrada_id_seq'::regclass), 350, 'uzitkova', 0);
+
+
+--Data tabulky zariadenie izby--
+INSERT INTO zariadenie_izby
+(id, id_izba, id_zariadenie, pocet)
+VALUES
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0)
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    (nextval('zariadenie_izby_id_seq'::regclass), 0, 0, 0),
+    v
+ 
